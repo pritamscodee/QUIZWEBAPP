c25b0e6f82745d2fae624fa8fcb1f6a36cbf0382
kuNA78 <tagoreacademy123@gmail.com>
2026-04-18 14:21:37 +0530

    Fix formatting issues in HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -62,6 +62,7 @@ export default function HomePage() {
                   </NeoButton>
                 </Link>
               )}
+              
             </div>
           </NeoCard>
         </div>
